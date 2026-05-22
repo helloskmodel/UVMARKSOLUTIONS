2e6920290cdb9d949b0c99c6739bc10fc6e820a1
helloskmodel <happysunkai2018@gmail.com>
2026-05-23 01:58:12 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,19 @@ import CaseStudies from "./pages/CaseStudies";
 import Contact from "./pages/Contact";
 
 export default function App() {
+  const path = window.location.pathname;
+
+  let Page = Home;
+
+  if (path === "/about") Page = About;
+  if (path === "/products") Page = Products;
+  if (path === "/case-studies") Page = CaseStudies;
+  if (path === "/contact") Page = Contact;
+
   return (
     <>
       <Navbar />
-      <Home />
-      <About />
-      <Products />
-      <CaseStudies />
-      <Contact />
+      <Page />
       <Footer />
     </>
   );
