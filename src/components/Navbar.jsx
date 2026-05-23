a29495c013d643397200cd16c67f6dc096f04dfd
helloskmodel <happysunkai2018@gmail.com>
2026-05-23 15:50:46 +0800

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,13 @@ export default function Navbar() {
   return (
     <header className="navbar">
       <div className="logo">
-        <img src="/logo.png" alt="UVMARK" />
+        <img src="/uvmark-logo.png" alt="UVMARK" />
       </div>
 
       <nav>
         <a href="/">HOME</a>
         <a href="/about">ABOUT</a>
-        <a href="/products">PRODUCTS</a>
+        <a href="/products">PRODUCT AND SERVICE</a>
         <a href="/case-studies">CASE STUDIES</a>
         <a href="/contact">CONTACT</a>
       </nav>
