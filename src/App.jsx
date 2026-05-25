a8f96d40d47a277371a2c0a4f96cb8970a3ed201
helloskmodel <happysunkai2018@gmail.com>
2026-05-25 14:23:56 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,9 @@
-import "./styles/Global.css";
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-
-import Home from "./pages/Home";
-import PrivacyPolicy from "./pages/PrivacyPolicy";
-import TermsOfUse from "./pages/TermsOfUse";
+import React from "react";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="/terms-of-use" element={<TermsOfUse />} />
-      </Routes>
-
-      <Footer />
-    </BrowserRouter>
+    <div style={{ padding: "120px", fontSize: "40px", color: "green" }}>
+      UVMARK TEST
+    </div>
   );
 }
