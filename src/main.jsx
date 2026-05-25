8386538e9a3c8bd45c715284c01e69324287cabf
helloskmodel <happysunkai2018@gmail.com>
2026-05-25 13:40:58 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles/Global.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+document.getElementById("root").innerHTML = `
+  <div style="padding:200px;font-size:40px;color:red;">
+    UVMARK MAIN TEST 888
+  </div>
+`;
