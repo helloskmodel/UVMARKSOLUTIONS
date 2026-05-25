3628ed38f098bd334ea2b3bc315da75bb8147e45
helloskmodel <happysunkai2018@gmail.com>
2026-05-25 12:58:04 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,31 @@
+import { Link } from "react-router-dom";
 import "../styles/Footer.css";
 
 export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
+
         <p>
           Houston · New York · Vancouver
         </p>
 
         <div className="footer-links">
-          <a href="/privacy-policy">Privacy Policy</a>
-          <a href="/terms-of-use">Terms of Use</a>
+
+          <Link to="/privacy-policy">
+            Privacy Policy
+          </Link>
+
+          <Link to="/terms-of-use">
+            Terms of Use
+          </Link>
+
         </div>
 
         <p className="footer-copy">
           © 2026 UVMARK Solutions. All rights reserved.
         </p>
+
       </div>
     </footer>
   );
