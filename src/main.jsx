fb083c4256dde9082ba208c74d0f4ef4509d3c05
helloskmodel <happysunkai2018@gmail.com>
2026-05-25 13:45:31 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <div style={{ padding: "200px", fontSize: "40px", color: "blue" }}>
-    REACT TEST 777
-  </div>
+  <App />
 );
