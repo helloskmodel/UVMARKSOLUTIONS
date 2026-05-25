cdccfaeae69c6128421f4ad9cfad223396044f61
helloskmodel <happysunkai2018@gmail.com>
2026-05-25 13:31:40 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,10 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
-
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Products from "./pages/Products";
-import ProductsServices from "./pages/ProductsServices";
-import CaseStudies from "./pages/CaseStudies";
-import Contact from "./pages/Contact";
-
-import PrivacyPolicy from "./pages/PrivacyPolicy";
-import TermsOfUse from "./pages/TermsOfUse";
+import React from "react";
 
 function App() {
   return (
-    <HashRouter>
-      <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products-services" element={<ProductsServices />} />
-        <Route path="/case-studies" element={<CaseStudies />} />
-        <Route path="/contact" element={<Contact />} />
-
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="/terms-of-use" element={<TermsOfUse />} />
-      </Routes>
-
-      <Footer />
-    </HashRouter>
+    <div style={{ padding: "200px", fontSize: "40px" }}>
+      UVMARK WEBSITE TEST
+    </div>
   );
 }
 
