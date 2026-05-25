f6ed1c29a95e9c5ad544b433dc5d04123633e0e0
helloskmodel <happysunkai2018@gmail.com>
2026-05-25 13:46:12 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div style={{ padding: "200px", fontSize: "40px", color: "red" }}>
-      UVMARK RED TEST 999
+    <div style={{ padding: "200px", fontSize: "40px", color: "green" }}>
+      APP TEST 666
     </div>
   );
 }
