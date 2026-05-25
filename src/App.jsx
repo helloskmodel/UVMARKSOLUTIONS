09c99580c0e74100052948c2d136c45af34836fe
helloskmodel <happysunkai2018@gmail.com>
2026-05-25 14:00:34 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import "./styles/Global.css";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+
 export default function App() {
   return (
-    <div style={{ padding: "200px", fontSize: "40px", color: "green" }}>
-      APP TEST 666
-    </div>
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
   );
 }
