104069a09d0abab7fae8178c59a299a288f678ba
helloskmodel <happysunkai2018@gmail.com>
2026-05-25 13:47:57 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-function App() {
+export default function App() {
   return (
     <div style={{ padding: "200px", fontSize: "40px", color: "green" }}>
       APP TEST 666
     </div>
   );
 }
-
-export default App;
